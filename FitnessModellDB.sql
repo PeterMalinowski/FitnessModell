de52c93b1b4128e34ab7e385b56bee172d9a89f3
Peter Malinowski <113600140+PeterMalinowski@users.noreply.github.com>
2023-01-17 19:56:57 +0100

    DB.sql update

--- FitnessModellDB.sql
+++ FitnessModellDB.sql
@@ -3,22 +3,20 @@ DROP TABLE IF EXISTS	Trainer;
 DROP TABLE IF EXISTS	Exercises;
 DROP TABLE IF EXISTS	Workout;
 DROP TABLE IF EXISTS	[User];
+DROP TABLE IF EXISTS 	UserWorkout;
 
 CREATE TABLE [User] (
 	Id 		  		INTEGER 		IDENTITY(1,1) PRIMARY KEY,	-- auto increment
 	Name 	  		VARCHAR(255)  	NOT NULL,
 	PhoneNumber		VARCHAR(16) 	NOT NULL,
-	WorkoutId		INTEGER			NOT NULL,
 	TrainerId		INTEGER			NOT NULL,
 	MembershipId	INTEGER 		NOT NULL,
-	FOREIGN KEY (WorkoutId) 		REFERENCES Workout(Id),
 	FOREIGN KEY (TrainerId) 		REFERENCES Trainer(Id),
-	FOREIGN KEY (MembershipId) 		REFERENCES MembershipId),
+	FOREIGN KEY (MembershipId) 		REFERENCES Membership(Id)
 );
 
 CREATE TABLE Workout (
 	Id 		  			INTEGER 		IDENTITY(1,1) PRIMARY KEY,	-- auto increment
-	[Date] 	  			DATE  			NOT NULL,
 	NumOfExercises		INTEGER			NOT NULL,
 	WorkoutDuration		VARCHAR(10)		NOT NULL,
 	ExercisesId			INTEGER			NOT NULL,
@@ -32,7 +30,7 @@ CREATE TABLE Exercises (
 	MuscleGroup		VARCHAR(255) 	NOT NULL,
 	[Sets]			INTEGER			NOT NULL,
 	Repetitions 	INTEGER  		NOT NULL,
-	Description		VARCHAR(255),
+	Description		VARCHAR(255)
 );
 
 CREATE TABLE Trainer (
@@ -49,7 +47,9 @@ CREATE TABLE Membership(
 	Price  			FLOAT 		NOT NULL
 );
 
-
-
-
-
+CREATE TABLE UserWorkout(
+	UserId			INTEGER		IDENTITY(1,1) PRIMARY KEY,
+	WorkoutId       INTEGER		IDENTITY(1,1) PRIMARY KEY,
+	FOREIGN KEY (UserId)		REFERENCES [User](Id),
+	FOREIGN KEY (WorkoutId)		REFERENCES Workout(Id)
+);
