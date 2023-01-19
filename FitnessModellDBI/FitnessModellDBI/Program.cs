c6077016c673e17c59413e07f16d94caa7f82b8d
PeterMalinowski <113600140+PeterMalinowski@users.noreply.github.com>
2023-01-19 13:01:37 +0100

    VisualStudio datei abgegeben

---
+++ FitnessModellDBI/FitnessModellDBI/Program.cs
@@ -0,0 +1,33 @@
+﻿using FitnessModellDBI.Models;
+
+using var db = new FitnessDBContext();
+
+Console.WriteLine("Add a new Excersie!");
+//var car = new Car() { Brand = "Porsche", Color = "Grün", EngineType = "Verbenner", Mileage=0, ModelName="911", ModelYear = 2000};
+Console.WriteLine("Name");
+var name = Console.ReadLine();
+Console.WriteLine("MuscleGroup");
+var muscleGroup = Console.ReadLine();
+
+Console.WriteLine("Sets");
+int sets = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Repetitions");
+var repetitions = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Description");
+var descripion = Console.ReadLine();
+
+
+var exercise = new Exercise()
+{
+    Name = name,
+    MuscleGroup = muscleGroup,
+    Sets = sets,
+    Repetitions = repetitions,
+    Description = descripion
+};
+
+db.Exercises.Add(exercise);
+db.SaveChanges();
+Console.WriteLine(exercise.Id);
