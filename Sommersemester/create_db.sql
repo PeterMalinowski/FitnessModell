9a399c57c902f67da790862e8aa85d84e9fd8c5d
PeterMalinowski <113600140+PeterMalinowski@users.noreply.github.com>
2023-06-05 12:15:33 +0200

    View created

--- Sommersemester/create_db.sql
+++ Sommersemester/create_db.sql
@@ -50,6 +50,7 @@ CREATE TABLE Exercise (
     Repetitions INT NOT NULL,
     OverExerciseId INT,
     Description NVARCHAR(100),
+    ExcerciseAttributes VARCHAR(MAX) NOT NULL,
     PRIMARY KEY (Id),
     FOREIGN KEY (OverExerciseId) REFERENCES Exercise(Id)
 );
@@ -70,3 +71,25 @@ CREATE TABLE UserWorkout (
     FOREIGN KEY (UserId) REFERENCES [User](Id),
     FOREIGN KEY (WorkoutId) REFERENCES Workout(Id)
 );
+
+CREATE VIEW OverExcercise AS 
+SELECT e.Id AS ExerciseId, e.Name, o.Id AS OverExerciseId, o.Name AS OverExerciseName
+FROM Exercise e
+LEFT JOIN Exercise o ON e.OverExerciseId = o.Id;
+
+INSERT INTO Exercise (Id, Name, MuscleGroup, Sets, Repetitions, OverExerciseId, Description, ExcerciseAttributes)
+VALUES
+    (1, 'Push-ups', 'Chest', 3, 10, 1, 'Basic bodyweight exercise', 'Difficulty: Beginner'),
+    (2, 'Squats', 'Legs', 4, 12, 1, 'Compound lower-body exercise', 'Difficulty: Intermediate'),
+    (3, 'Bench Press', 'Chest', 4, 8, 2, 'Strength training exercise', 'Difficulty: Advanced'),
+    (4, 'Deadlifts', 'Back', 3, 6, 2, 'Full-body compound movement', 'Difficulty: Advanced'),
+    (5, 'Shoulder Press', 'Shoulders', 3, 10, 1, 'Upper-body pressing exercise', 'Difficulty: Intermediate'),
+    (6, 'Bicep Curls', 'Arms', 3, 12, NULL, 'Isolation exercise for biceps', 'Difficulty: Beginner'),
+    (7, 'Plank', 'Core', 2, 60, NULL, 'Core-strengthening exercise', 'Difficulty: Beginner'),
+    (8, 'Lunges', 'Legs', 3, 10, NULL, 'Lower-body exercise', 'Difficulty: Beginner'),
+    (9, 'Dumbbell Flyes', 'Chest', 3, 12, 3, 'Isolation exercise for chest', 'Difficulty: Intermediate'),
+    (10, 'Leg Press', 'Legs', 3, 15, 2, 'Compound leg exercise', 'Difficulty: Intermediate'),
+    (11, 'Pull-ups', 'Back', 3, 8, 4, 'Upper-body compound exercise', 'Difficulty: Intermediate'),
+    (12, 'Arnold Press', 'Shoulders', 3, 10, 5, 'Dumbbell shoulder exercise', 'Difficulty: Intermediate');
+
+SELECT * FROM OverExcercise;
